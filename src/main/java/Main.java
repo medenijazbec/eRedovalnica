57b87d94994de9b2727e0350934718835d429d05
Matic Ozimic <matic.ozimic@gmail.com>
2021-03-23 12:56:14 +0100

    dodan main form kjer so bojo dodajale ocene

---
+++ src/main/java/Main.java
@@ -0,0 +1,32 @@
+import javax.swing.*;
+
+public class Main
+{
+    public static void main(String[] args) throws ClassNotFoundException, UnsupportedLookAndFeelException, InstantiationException, IllegalAccessException {
+        UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+
+
+        SwingUtilities.invokeLater(new Runnable() {
+            @Override
+            public void run()
+            {
+                LoginForm loginForm = new LoginForm();
+                loginForm.setVisible(true);
+
+                mainForm MainForm = new mainForm();
+                MainForm.setVisible(true);
+
+
+            }
+        });
+
+
+
+
+
+
+
+    }
+
+
+}
