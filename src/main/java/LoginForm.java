c6e3daa71d553d9267b03f71f85e03c42baecf2a
Žan <zan.tramte@gmail.com>
2021-03-30 13:31:53 +0200

    Changing display message in login form.

--- src/main/java/LoginForm.java
+++ src/main/java/LoginForm.java
@@ -24,7 +24,7 @@ public class LoginForm extends JFrame
         {
             @Override
             public void actionPerformed(ActionEvent e) {
-                JOptionPane.showMessageDialog(rootPane, "Button goes click");
+                JOptionPane.showMessageDialog(rootPane, "Registracija uspešna!");
             }
         });
 
