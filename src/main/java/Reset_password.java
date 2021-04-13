5e53ae488b29203442602a379339cab8ffd1083b
Žan <zan.tramte@gmail.com>
2021-04-13 11:49:55 +0200

    Adding database connection, changing to class Database.java.

--- src/main/java/Reset_password.java
+++ src/main/java/Reset_password.java
@@ -1,7 +1,23 @@
 import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class Reset_password
 {
     private JTextField textField1;
     private JPasswordField passwordField1;
+    private JButton ponastaviGesloButton;
+
+    public Reset_password()
+    {
+        ponastaviGesloButton.addActionListener(new ActionListener()
+        {
+            @Override
+            public void actionPerformed(ActionEvent e)
+            {
+                
+
+            }
+        });
+    }
 }
