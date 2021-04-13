cfb43887ddc020eda9dcb0d98589131aa66f7fc5
Matic Ozimic <Matic Ozimic@DESKTOP-30HHFTI>
2021-04-13 11:42:31 +0200

    dodan class ucitelji

--- src/main/java/classes.java
+++ src/main/java/classes.java
@@ -154,7 +154,36 @@ public class classes {
         }
     }
 
+    public class ucitelji
+    {
+        int id_st;
+        String ime;
+        String priimek;
+        String email;
+        String geslo;
+        String telefon;
+        int razred_id;
+        int kraj_id;
 
+        public ucitelji(int _id_st, String _ime, String _priimek, String _email, String _geslo, String _telefon, int _razred_id, int _kraj_id)
+        {
+            id_st=_id_st;
+            ime=_ime;
+            priimek=_priimek;
+            email=_email;
+            geslo=_geslo;
+            telefon=_telefon;
+            razred_id=_razred_id;
+            kraj_id=_kraj_id;
+        }
+
+        public String uciteljiString()
+        {
+            String list;
+            list=(id_st+" "+ime+" "+priimek+" "+email+" "+geslo+" "+telefon+" "+kraj_id+" "+razred_id);
+            return list;
+        }
+    }
 
 
 }
